6f88787f821ee7c6cd3a750839568486f81a3d7b
Nicolas Silva do Sacramento <36601217+nih71@users.noreply.github.com>
2018-04-28 17:18:36 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ Definition of views.
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
-from app.models import Curso, Vestibular
+from app.models import Curso, Vestibular, Candidato
 from datetime import datetime
 
 def home(request):
@@ -77,3 +77,16 @@ def cadastro_vestibulares(request):
         })
 
 )
+def inscricao_candidatos(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/inscricao_candidatos.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'inscricao de candidatoss',
+            'candidatos': Candidato.objects.all(),
+            'year':datetime.now().year,
+        })
+    )
+
